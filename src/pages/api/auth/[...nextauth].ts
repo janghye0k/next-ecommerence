388c99e53401d4311c2bdef28b9273058a7d932c
D0OR_LABTOP\d0oRh <d0oR.hyeok@gmail.com>
2023-02-26 23:32:19 +0900

    auth 작업중 - prisma 접속 라이브러리 파일 생성 - provider 종류별 로그인 redirect 구현 - credentials 로그인 authorize 틀 잡았음 (비밀번호 암호와 대조 미구현)

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,8 +2,9 @@ import NextAuth from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
 import KakaoProvider from 'next-auth/providers/kakao'
 import GoogleProvider from 'next-auth/providers/google'
+import prisma from '@/lib/prisma'
 
-export const authOptions = {
+export default NextAuth({
   secret: process.env.NEXTAUTH_SECRET,
   /**
   pages: {
@@ -14,23 +15,53 @@ export const authOptions = {
     newUser: '/auth/new-user', // New users will be directed here on first sign in (leave the property out if not of interest)
   },
    */
-  /**
   callbacks: {
     async signIn({ user, account, profile, email, credentials }) {
-      return true
-    },
-    async redirect({ url, baseUrl }) {
-      return baseUrl
-    },
-    async session({ session, token, user }) {
-      return session
+      const provider = account?.provider as 'kakao' | 'google' | 'credentials'
+
+      // Case provider is 'credentials', the authorize function will perform verification, so return true.
+      // 프로바이더가 'credentials'인 경우는 authorize 함수에서 검증을 진행하기에 true를 리턴한다.
+      if (provider === 'credentials') return true
+
+      // Case social login, check if there is a user, and if not, redirect to account page.
+      // 소셜로그인일 경우 유저가 있는지 확인하고 없다면 회원가입 페이지로 이동시킨다.
+      const findUser = await prisma.user.findUnique({
+        where: { email: user.email || '-1' },
+      })
+      return (
+        Boolean(findUser) ||
+        `/account?redirect_from=signin&provider=${provider}`
+      )
     },
     async jwt({ token, user, account, profile, isNewUser }) {
+      console.log('@@@ JWT @@@')
+      console.log('token', token)
+      console.log('user', user)
+      console.log('account', account)
+      console.log('profile', profile)
+      console.log('isNewUser', isNewUser)
       return token
     },
+    async session({ session, token, user }) {
+      console.log('@@@ Session @@@')
+      console.log('session', session)
+      console.log('token', token)
+      console.log('user', user)
+      return session
+    },
   },
-   */
   providers: [
+    // 카카오 로그인
+    KakaoProvider({
+      clientId: process.env.KAKAO_ID,
+      clientSecret: process.env.KAKAO_SECRET,
+    }),
+    // 구글 로그인
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+    // 일반 로그인
     CredentialsProvider({
       name: 'Credentials',
       credentials: {
@@ -46,27 +77,19 @@ export const authOptions = {
         },
       },
       authorize: async function authorize(credentials, req) {
-        const user = { id: '1', name: 'J Smith', email: 'jsmith@example.com' }
-        if (!user) {
-          // If you return null then an error will be displayed advising the user to check their details.
-          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
-          return null
-        }
-        // Any object returned will be saved in `user` property of the JWT
-        return user
+        // DB를 조회하여 유저 존재여부를 확인하고 비밀번호를 대조하여 올바른 접속인지 확인한다.
+        if (!credentials) return null
+        const { email, password } = credentials
+        const user = await prisma.user.findUnique({
+          where: { email },
+        })
+
+        /** @todo 비밀번호 암호화 비교 */
+        const encodePassword = password
+
+        const isNotMatch = !user || user.pwd !== encodePassword
+        return isNotMatch ? user : null
       },
     }),
-    // 카카오 로그인
-    KakaoProvider({
-      clientId: process.env.KAKAO_ID,
-      clientSecret: process.env.KAKAO_SECRET,
-    }),
-    // 구글 로그인
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-    }),
   ],
-}
-
-export default NextAuth(authOptions)
+})
--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,6 +1,20 @@
-import NextAuth, { DefaultSession } from 'next-auth'
+import NextAuth, { DefaultSession, DefaultUser } from 'next-auth'
+
+type NextAuthUser = DefaultUser
 
 declare module 'next-auth' {
+  interface User extends DefaultUser {
+    foo?: string
+  }
+  /**
+   * Usually contains information about the provider being used
+   * and also extends `TokenSet`, which is different tokens returned by OAuth Providers.
+   */
+  interface Account {
+    foo?: string
+  }
+  /** The OAuth profile returned from your provider */
+
   /**
    * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
    */
@@ -8,6 +22,6 @@ declare module 'next-auth' {
     user: {
       /** The user's postal address. */
       address?: string
-    } & DefaultSession['user']
+    } & DefaultSession
   }
 }
