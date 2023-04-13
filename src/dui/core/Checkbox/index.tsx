ddf88bd3124a11c03c31f32f5cf7fc195a072257
KRSOFT0723 <krsoft0723@gmail.com>
2023-04-13 11:08:28 +0900

    [feat] Checkbox - size, labelPosition, color, icon

---
+++ src/dui/core/Checkbox/index.tsx
@@ -0,0 +1,69 @@
+import React, { forwardRef } from 'react'
+import Dui from '@/dui/types'
+import cn from 'classnames/bind'
+import scss from '@/scss/dui/core/checkbox.module.scss'
+import { FaCheck } from 'react-icons/fa'
+
+const cx = cn.bind(scss)
+
+type CheckboxComponentKeys = 'root' | 'label' | 'checkbox'
+
+interface CheckboxProps
+  extends Omit<Dui.DefaultInputProps, 'type' | 'style' | 'size'> {
+  labelPosition?: 'left' | 'right'
+  classNames?: { [key in CheckboxComponentKeys]?: string }
+  styles?: { [key in CheckboxComponentKeys]?: React.CSSProperties }
+  size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl'
+  color?: ThemeColor | 'theme'
+  icon?: React.ReactNode
+}
+
+function BaseCheckbox(
+  {
+    className,
+    classNames,
+    children,
+    labelPosition = 'right',
+    styles,
+    size = 'sm',
+    color,
+    icon,
+    ...props
+  }: CheckboxProps,
+  ref?: Dui.CoreRef<HTMLInputElement>,
+) {
+  const isLeft = labelPosition === 'left'
+  return (
+    <div
+      className={`${cx('root')}  ${className ?? ''} ${classNames?.root ?? ''}`}
+      style={styles?.root}
+    >
+      <label
+        className={`${cx('label')} ${classNames?.label ?? ''}`}
+        style={styles?.label}
+      >
+        {isLeft ? children : null}
+        <input
+          className={cx('input', size, { [color as string]: !!color })}
+          type="checkbox"
+          ref={ref}
+          {...props}
+        />
+        <span
+          className={`${cx('checkbox')} ${classNames?.checkbox ?? ''}`}
+          role="checkbox"
+          aria-checked={props.checked}
+          style={styles?.checkbox}
+        >
+          {icon ?? <FaCheck />}
+        </span>
+        {!isLeft ? children : null}
+      </label>
+    </div>
+  )
+}
+
+const Checkbox = forwardRef(BaseCheckbox)
+Checkbox.displayName = 'Checkbox'
+
+export default Checkbox
--- src/dui/core/index.tsx
+++ src/dui/core/index.tsx
@@ -1,4 +1,5 @@
 import Button from './Button'
+import Checkbox from './Checkbox'
 import Input from './Input'
 
-export { Button, Input }
+export { Button, Input, Checkbox }
