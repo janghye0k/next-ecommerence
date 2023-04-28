6e35d394d23c659495a0737a8eaaa828ff621d31
KRSOFT0723 <krsoft0723@gmail.com>
2023-04-28 14:56:26 +0900

    [feat] feedback/Alert - color, size, variant, closebutton, icon

---
+++ src/dui/feedback/Alert/index.tsx
@@ -0,0 +1,77 @@
+import React, { ReactNode } from 'react'
+import scss from '@/scss/dui/feedback/alert.module.scss'
+import cn from 'classnames/bind'
+import Dui from '@/dui/types'
+import { MdClose } from 'react-icons/md'
+
+type ComponentClassName = 'root' | 'icon' | 'body' | 'title' | 'content'
+
+export interface AlertProps extends Omit<Dui.DefaultDivProps, 'title'> {
+  color?: ThemeColor
+  icon?: ReactNode
+  withCloseButton?: boolean
+  title?: ReactNode
+  classNames?: { [key in ComponentClassName]?: string }
+  size?: 'sm' | 'md' | 'lg'
+  variant?: 'light' | 'outline' | 'filled'
+  closeButtonProps?: Omit<
+    React.DetailedHTMLProps<
+      React.HTMLAttributes<HTMLSpanElement>,
+      HTMLSpanElement
+    >,
+    'ref'
+  >
+}
+
+const cx = cn.bind(scss)
+
+function BaseAlert(
+  {
+    classNames = {},
+    className = '',
+    children,
+    color = 'blue',
+    size = 'md',
+    icon,
+    title,
+    variant = 'light',
+    closeButtonProps = {},
+    withCloseButton = false,
+    ...props
+  }: AlertProps,
+  ref?: Dui.CoreRef<HTMLDivElement>,
+) {
+  return (
+    <div
+      ref={ref}
+      className={`${cx('root', color, size, variant)} ${
+        classNames.root ?? ''
+      } ${className}`}
+      {...props}
+    >
+      <div className={`${cx('body')} ${classNames.body ?? ''}`}>
+        {!icon ? null : (
+          <div className={`${cx('icon')} ${classNames.icon ?? ''}`}>{icon}</div>
+        )}
+        <div className={cx('container')}>
+          <div className={`${cx('title')} ${classNames.title ?? ''}`}>
+            <span>{title}</span>
+            {!withCloseButton ? null : (
+              <span className={cx('close')} {...closeButtonProps}>
+                <MdClose />
+              </span>
+            )}
+          </div>
+          <div className={`${cx('content')} ${classNames.content ?? ''}`}>
+            {children}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const Alert = React.forwardRef(BaseAlert)
+Alert.displayName = 'Alert'
+
+export default Alert
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,24 @@
+import { Switch } from '@/dui/core'
+import { Alert } from '@/dui/feedback'
 import Head from 'next/head'
-import { useState } from 'react'
+import { FaWater } from 'react-icons/fa'
 
 export default function Home() {
-  const [open, setOpen] = useState(false)
-
-  function toggle() {
-    setOpen((state) => !state)
+  function onChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const $html = document.querySelector('html')
+    if ($html)
+      $html.dataset.theme = event.currentTarget.checked ? 'dark' : 'light'
   }
   return (
     <>
       <Head>
         <title>Home | PIIC</title>
       </Head>
+      <Switch onChange={onChange} />
       <main>Welcome</main>
+      <Alert icon={<FaWater />} title="test">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque, magni?
+      </Alert>
     </>
   )
 }
